e9328b45318ac9155a40e1e317d59c693f83da27
natalia <lnataliaperri@gmail.com>
2023-11-20 14:08:44 -0300

    Implementado zod e useForm em formulário de atualizar dados de usuário.
    Co-authored-by: Pedro Samuel <Pedro-Cecilio@users.noreply.github.com>
    Co-authored-by: João Vítor Santos de Andrade <JoaoVitorSantDrade@users.noreply.github.com>

--- src/components/UpdateUserModal.tsx
+++ src/components/UpdateUserModal.tsx
@@ -1,53 +1,104 @@
-import { ChangeEvent, useState } from "react";
+import { z } from "zod";
 import { UserDetailsData } from "../@types/UserDetailsData.type";
-import closeIcon from "../assets/closeIcon.svg"
+import closeIcon from "../assets/closeIcon.svg";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Id } from "react-toastify";
 
 interface UpdateUserProps {
-    user: UserDetailsData;
-    handleUpdateModal:()=>void;
+  user: UserDetailsData;
+  handleUpdateModal: () => void;
+  emmitErrorToast: (message: string, duration: number) => Id;
 }
 
-const UpdateUserModal = ({ user, handleUpdateModal }: UpdateUserProps) => {
-    const [name, setName] = useState(user.name)
-    const [lastName, setLastName] = useState(user.lastName)
-    const [password, setPassword] = useState("")
+const UpdateUserModal = ({
+  user,
+  handleUpdateModal,
+  emmitErrorToast,
+}: UpdateUserProps) => {
+  const updateUserFormSchema = z.object({
+    name: z.string(),
+    lastName: z.string(),
+    password: z.string().min(8),
+  });
 
-    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value);
-    };
+  type updateUserInputs = z.infer<typeof updateUserFormSchema>;
 
-    const handleLastNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setLastName(e.target.value);
-    };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<updateUserInputs>({
+    resolver: zodResolver(updateUserFormSchema),
+  });
 
-    const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-    };
-    return (
-        user && (
-            <div className="absolute h-full w-full bg-white">
-                <img onClick={handleUpdateModal} className=" absolute right-1 top-1 w-10 cursor-pointer" src={closeIcon} alt="" />
-                <div className="flex flex-col items-center mt-[150px] font-[Content]">
-                    <div className="w-[70%] flex flex-col items-center">
-                        <div className="w-full max-w-[500px]">
-                            <h1 className="text-blue-950 text-3xl font-bold font-['Content']">Editar perfil</h1>
-                        </div>
-                        <form className="flex flex-col gap-3 w-full max-w-[500px] mt-16">
-                            <label className="font-semibold text-blue-950" htmlFor="name">Nome:</label>
-                            <input className="p-2 bg-white rounded-lg border border-neutral-400" type="text" value={name} id="name" onChange={handleNameChange} />
-                            <label className="font-semibold text-blue-950" htmlFor="lastname">Sobrenome:</label>
-                            <input className="p-2 bg-white rounded-lg border border-neutral-400" type="text" value={lastName} id="lastname" onChange={handleLastNameChange} />
-                            <label className="font-semibold text-blue-950" htmlFor="password">Senha:</label>
-                            <input className="p-2 bg-white rounded-lg border border-neutral-400" type="password" value={password} id="passworc" onChange={handlePasswordChange} />
-                            <div className="text-blue-950 text-[14px] ml-4 font-normal">Sua senha deve ter no mínimo 8 caracteres.</div>
-                        </form>
-                        <div className="mt-32 mb-4 self-center w-[50%] max-w-[300px] p-2 text-center bg-white rounded-lg border border-rose-400 text-rose-400 text-[14px] font-bold hover:bg-pink-salmon hover:text-white cursor-pointer">Atualizar usuário</div>
+  function handleErrors() {}
 
-                    </div>
-                </div>
+  async function handleUpdateUser(data: updateUserInputs) {}
+
+  return (
+    user && (
+      <div className="absolute h-full w-full bg-white">
+        <img
+          onClick={handleUpdateModal}
+          className=" absolute right-1 top-1 w-10 cursor-pointer"
+          src={closeIcon}
+          alt=""
+        />
+        <div className="flex flex-col items-center mt-[150px] font-[Content]">
+          <div className="w-[70%] flex flex-col items-center">
+            <div className="w-full max-w-[500px]">
+              <h1 className="text-blue-950 text-3xl font-bold font-['Content']">
+                Editar perfil
+              </h1>
             </div>
-        )
+            <form
+              onSubmit={handleSubmit(handleUpdateUser)}
+              className="flex flex-col gap-3 w-full max-w-[500px] mt-16"
+            >
+              <label className="font-semibold text-blue-950" htmlFor="name">
+                Nome:
+              </label>
+              <input
+                className="p-2 bg-white rounded-lg border border-neutral-400"
+                type="text"
+                id="name"
+                {...register("name")}
+              />
+              <label className="font-semibold text-blue-950" htmlFor="lastname">
+                Sobrenome:
+              </label>
+              <input
+                className="p-2 bg-white rounded-lg border border-neutral-400"
+                type="text"
+                id="lastname"
+                {...register("lastName")}
+              />
+              <label className="font-semibold text-blue-950" htmlFor="password">
+                Senha:
+              </label>
+              <input
+                className="p-2 bg-white rounded-lg border border-neutral-400"
+                type="password"
+                id="password"
+                {...register("password")}
+              />
+              <div className="text-blue-950 text-[14px] ml-4 font-normal">
+                Sua senha deve ter no mínimo 8 caracteres.
+              </div>
+            </form>
+            <button
+              type="submit"
+              onClick={() => handleErrors()}
+              className="mt-32 mb-4 self-center w-[50%] max-w-[300px] p-2 text-center bg-white rounded-lg border border-rose-400 text-rose-400 text-[14px] font-bold hover:bg-pink-salmon hover:text-white cursor-pointer"
+            >
+              Atualizar usuário
+            </button>
+          </div>
+        </div>
+      </div>
     )
-}
+  );
+};
 
 export default UpdateUserModal;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -3,64 +3,81 @@ import { useUserDetails } from "../hooks/useUserDetails";
 import { useEffect, useState } from "react";
 import { UserDetailsData } from "../@types/UserDetailsData.type";
 import Header from "../components/Header";
-import person from "../assets/person.svg"
+import person from "../assets/person.svg";
 import { useNavigate } from "react-router-dom";
 import UpdateUserModal from "../components/UpdateUserModal";
-
+import { useToast } from "../hooks/useToast";
 
 const Profile = () => {
-    const [cookies, , removeCookie] = useCookies();
-    const token = cookies["token"];
-    const { getUser } = useUserDetails();
-    const [user, setUser] = useState<UserDetailsData>();
-    const [openUpdateModal, setOpenUpdateModal] = useState(false);
-    const navigate = useNavigate();
+  const [cookies, , removeCookie] = useCookies();
+  const token = cookies["token"];
+  const { getUser } = useUserDetails();
+  const [user, setUser] = useState<UserDetailsData>();
+  const [openUpdateModal, setOpenUpdateModal] = useState(false);
+  const navigate = useNavigate();
+  const { emmitErrorToast, Toast } = useToast();
 
-    const logout = () => {
-        removeCookie("token");
-        navigate("/login");
+  const logout = () => {
+    removeCookie("token");
+    navigate("/login");
+  };
+  const handleUpdateModal = () => {
+    setOpenUpdateModal((previous) => !previous);
+  };
+
+  useEffect(() => {
+    const defineUser = async () => {
+      const user = await getUser(token);
+      setUser(user);
     };
-    const handleUpdateModal = () => {
-        setOpenUpdateModal(previous => !previous)
-    };
+    defineUser();
+  }, [user]);
 
-    useEffect(() => {
-        const defineUser = async () => {
-            const user = await getUser(token);
-            setUser(user);
-        }
-        defineUser();
-    }, [user]);
-
-    return (
-        user && (
-            <div className="relative flex flex-col h-screen">
-                <Header />
-                <main className="flex-1 flex flex-col items-center mt-[150px] font-[Content]">
-                    <div className="w-[70%] flex flex-col ">
-                        <div className="flex  justify-between items-center">
-                            <h1 className="text-2xl font-bold text-blue-950">Perfil</h1>
-                            <p onClick={handleUpdateModal} className="text-rose-400 hover:text-rose-300 cursor-pointer text-xs font-bold">Editar perfil</p>
-                        </div>
-                        <div className="flex items-center gap-8 mt-16 ml-[10%]">
-                            <div>
-                                <img className="w-[94.48px] h-[94.48px]" src={person} />
-                            </div>
-                            <div className="flex flex-col gap-3">
-                                <p className="text-black text-2xl font-bold">{user.name} {user.lastName}</p>
-                                <p className="text-black text-lg font-normal">{user.email}</p>
-                            </div>
-                        </div>
-                        <div onClick={logout} className="mt-32 mb-4 self-center w-[50%] max-w-[300px] p-2 text-center bg-white rounded-lg border border-rose-400 text-rose-400 text-[13px] font-bold hover:bg-pink-salmon hover:text-white cursor-pointer">Sair</div>
-
-                    </div>
-                </main>
-                {openUpdateModal && <UpdateUserModal user={user} handleUpdateModal={handleUpdateModal} />}
+  return (
+    user && (
+      <div className="relative flex flex-col h-screen">
+        <Header />
+        <main className="flex-1 flex flex-col items-center mt-[150px] font-[Content]">
+          <div className="w-[70%] flex flex-col ">
+            <div className="flex  justify-between items-center">
+              <h1 className="text-2xl font-bold text-blue-950">Perfil</h1>
+              <p
+                onClick={handleUpdateModal}
+                className="text-rose-400 hover:text-rose-300 cursor-pointer text-xs font-bold"
+              >
+                Editar perfil
+              </p>
             </div>
-        )
-
-
+            <div className="flex items-center gap-8 mt-16 ml-[10%]">
+              <div>
+                <img className="w-[94.48px] h-[94.48px]" src={person} />
+              </div>
+              <div className="flex flex-col gap-3">
+                <p className="text-black text-2xl font-bold">
+                  {user.name} {user.lastName}
+                </p>
+                <p className="text-black text-lg font-normal">{user.email}</p>
+              </div>
+            </div>
+            <div
+              onClick={logout}
+              className="mt-32 mb-4 self-center w-[50%] max-w-[300px] p-2 text-center bg-white rounded-lg border border-rose-400 text-rose-400 text-[13px] font-bold hover:bg-pink-salmon hover:text-white cursor-pointer"
+            >
+              Sair
+            </div>
+          </div>
+          <Toast />
+        </main>
+        {openUpdateModal && (
+          <UpdateUserModal
+            emmitErrorToast={emmitErrorToast}
+            user={user}
+            handleUpdateModal={handleUpdateModal}
+          />
+        )}
+      </div>
     )
-}
+  );
+};
 
 export default Profile;
