9f828b1e58a75b522e5e1c1b2d680e85585fbf7b
Pedro Samuel <cardev604@gmail.com>
2023-11-22 14:47:06 -0300

    Refatorando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,39 +27,31 @@ const Header = () => {
 
   return (
     <header className="w-full h-min-[300px] p-2  bg-blue-950 ">
-      <div className="flex items-center  justify-between max-[730px]:hidden">
+      <div className="flex items-center  justify-between">
+        <img
+          className="w-[45px] h-[45px] min-[730px]:hidden cursor-pointer"
+          src={menuOpen ? closeIconMenu : menu}
+          onClick={handleMenuOpen}
+        />
         <Link to="/">
           <img className="w-[54px] h-[55px]" src={logo} />
         </Link>
-        <div className="w-64 h-[29px] flex items-center gap-16">
+        <div className="w-64 h-[29px] max-[730px]:hidden flex items-center gap-16">
           <Link
             to="/"
-            className={`text-white text-base ${
-              pathname === "/" && "font-bold"
-            }  font-16`}
+            className={`text-white text-base ${pathname === "/" && "font-bold"
+              }  font-16`}
           >
             Minha cabeceira
           </Link>
           <Link
             to="/explore"
-            className={`text-white text-base font-sans  ${
-              pathname === "/explore" && "font-bold"
-            }`}
+            className={`text-white text-base font-sans  ${pathname === "/explore" && "font-bold"
+              }`}
           >
             Explorar
           </Link>
         </div>
-        <Link to="/profile">
-          <img className="w-[34px] h-9 " src={profile} />
-        </Link>
-      </div>
-      <div className="min-[730px]:hidden flex relative justify-between items-center">
-        <img
-          className="w-[45px] h-[45px]"
-          src={menuOpen ? closeIconMenu : menu}
-          onClick={handleMenuOpen}
-        />
-        <img className="w-[54px] h-[55px]" src={logo} />
         <Link to="/profile">
           <img className="w-[34px] h-9 " src={profile} />
         </Link>
